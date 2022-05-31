c50ed5a72607117193e0aa6c7c21350c8ae10644
schizohub <93917657+schizohub@users.noreply.github.com>
2022-05-31 23:58:16 +0500

    project files have been uploaded

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st 
+from fastai.vision.all import *
+import plotly.express as px
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+st.title('Classification of Boat,Car and Airplane')
+
+file = st.file_uploader('Upload picture', type=['jpeg','png','gif','svg'])
+if file:
+    st.image(file)
+
+    img = PILImage.create(file)
+
+    model = load_learner('transport_class_model.pkl')
+
+    pred,pred_id,probs = model.predict(img)
+    st.success(f"Prediction: {pred}")
+    st.info(f"Probability: {probs[pred_id]*100:.1f}%")
+
+    fig = px.bar(x=probs*100,y= model.dls.vocab)
+    st.plotly_chart(fig)
+
